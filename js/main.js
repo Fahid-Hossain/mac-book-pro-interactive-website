e3ced055a82a4bdb868235ad53f9b0dcfa9e320e
Fahid Hossain <fahidhossain2021@gmail.com>
2021-08-21 05:03:45 +0600

    renew site with function

--- js/main.js
+++ js/main.js
@@ -1,31 +1,8 @@
+//---//---///--get Memory cost by specification ----//-----//---
 
-// memory8GB button
-document.getElementById("memory8GB-btn").addEventListener("click",function(){
+function getMemoryCost(cost) {
     let memoryCost = document.getElementById("memory-cost");
-    memoryCost.innerText =0;
-
-     //fixed price
-     let totalCostId = document.getElementById("best-price");
-     let totalCost = parseInt(totalCostId.innerText);
-     //delivery cost 
-     let deliveryCost = document.getElementById("delivery-cost");
-     //storage cost
-     let storageCost = document.getElementById("storage-cost");
- 
-     // totalPrice
-     let totalPrice = document.getElementById("total-cost");
-     let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
-
-      // total amount
-   let totalAmount = document.getElementById("total-amount");
-   totalAmount.innerText = total;
-})
-
-
-//memory16GB button
-document.getElementById("memory16GB-btn").addEventListener("click",function(){
-    let memoryCost = document.getElementById("memory-cost");
-    memoryCost.innerText =180;
+    memoryCost.innerText = cost;
 
     //fixed price
     let totalCostId = document.getElementById("best-price");
@@ -39,108 +16,39 @@ document.getElementById("memory16GB-btn").addEventListener("click",function(){
     let totalPrice = document.getElementById("total-cost");
     let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
 
-     // total amount
-   let totalAmount = document.getElementById("total-amount");
-   totalAmount.innerText = total;
+    // total amount
+    let totalAmount = document.getElementById("total-amount");
+    totalAmount.innerText = total;
+}
 
-})
+// -----///----///--get Storage cost by specification -----///---////---
 
-////////////////////-----------------//////////////////////////
-// storage256GB
-document.getElementById("storage256-btn").addEventListener("click",function(){
+function getStorageCost(cost) {
     let storageCost = document.getElementById("storage-cost");
-    storageCost.innerText =0;
+    storageCost.innerText = cost;
 
-  //fixed price
-  let totalCostId = document.getElementById("best-price");
-  let totalCost = parseInt(totalCostId.innerText);
-  //memory cost 
-  let memoryCost = document.getElementById("memory-cost");
-  //delivery cost
-  let deliveryCost = document.getElementById("delivery-cost");
- 
-  // totalPrice
-  let totalPrice = document.getElementById("total-cost");
-  let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
-
-   // total amount
-   let totalAmount = document.getElementById("total-amount");
-   totalAmount.innerText = total;
-
-})
-// storage512GB
-document.getElementById("storage512-btn").addEventListener("click",function(){
-    let storageCost = document.getElementById("storage-cost");
-    storageCost.innerText = 100;
-
-  
- //fixed price
- let totalCostId = document.getElementById("best-price");
- let totalCost = parseInt(totalCostId.innerText);
- //memory cost 
- let memoryCost = document.getElementById("memory-cost");
- //delivery cost
- let deliveryCost = document.getElementById("delivery-cost");
-
- // totalPrice
- let totalPrice = document.getElementById("total-cost");
- let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
-
-  // total amount
-  let totalAmount = document.getElementById("total-amount");
-  totalAmount.innerText = total;
-
-
-})
-// storage1000GB
-document.getElementById("storage1000-btn").addEventListener("click",function(){
-    let storageCost = document.getElementById("storage-cost");
-    storageCost.innerText = 180;
-
- //fixed price
- let totalCostId = document.getElementById("best-price");
- let totalCost = parseInt(totalCostId.innerText);
- //memory cost 
- let memoryCost = document.getElementById("memory-cost");
- //delivery cost
- let deliveryCost = document.getElementById("delivery-cost");
-
- // totalPrice
- let totalPrice = document.getElementById("total-cost");
- let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
-
-  // total amount
-  let totalAmount = document.getElementById("total-amount");
-  totalAmount.innerText = total;
-})
-
-/////////////////////----------------------////////////////////////
-
-//delivery25Aug
-document.getElementById("delivery25-btn").addEventListener("click",function(){
+    //fixed price
+    let totalCostId = document.getElementById("best-price");
+    let totalCost = parseInt(totalCostId.innerText);
+    //memory cost 
+    let memoryCost = document.getElementById("memory-cost");
+    //delivery cost
     let deliveryCost = document.getElementById("delivery-cost");
-    deliveryCost.innerText = 0;
 
-   //fixed price
-   let totalCostId = document.getElementById("best-price");
-   let totalCost = parseInt(totalCostId.innerText);
-   //memory cost 
-   let memoryCost = document.getElementById("memory-cost");
-   //storage cost
-   let storageCost = document.getElementById("storage-cost");
-
-   // totalPrice
-   let totalPrice = document.getElementById("total-cost");
-   let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
+    // totalPrice
+    let totalPrice = document.getElementById("total-cost");
+    let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
 
     // total amount
     let totalAmount = document.getElementById("total-amount");
     totalAmount.innerText = total;
-})
-//delivery25Aug
-document.getElementById("delivery21-btn").addEventListener("click",function(){
+}
+
+// ---- // ---get Delivery cost by specific date---//---//--
+
+function getDeliveryCost(cost) {
     let deliveryCost = document.getElementById("delivery-cost");
-    deliveryCost.innerText = 20;
+    deliveryCost.innerText = cost;
 
     //fixed price
     let totalCostId = document.getElementById("best-price");
@@ -152,36 +60,77 @@ document.getElementById("delivery21-btn").addEventListener("click",function(){
 
     // totalPrice
     let totalPrice = document.getElementById("total-cost");
-   let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
-   
-   // total amount
-   let totalAmount = document.getElementById("total-amount");
-   totalAmount.innerText = total;
+    let total = totalPrice.innerText = totalCost + parseInt(deliveryCost.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText);
 
+    // total amount
+    let totalAmount = document.getElementById("total-amount");
+    totalAmount.innerText = total;
+}
+
+
+///////------------/////////////////--memory button click--------------////////////////////
+
+// memory8GB button
+document.getElementById("memory8GB-btn").addEventListener("click", function () {
+    getMemoryCost(0);
 })
 
-// promo code 
 
-document.getElementById("promo-btn").addEventListener("click",function(){
- let promoInput = document.getElementById("promo-input");
- let promoInputValue = promoInput.value ;
- //total price
- let totalAmountId = document.getElementById("total-amount");
- let totalAmount = totalAmountId.innerText;
-//  console.log(totalAmount.innerText);
-let discount = (totalAmount * 20) /100;
-let discountAmount = totalAmount - discount ;
+//memory16GB button
+document.getElementById("memory16GB-btn").addEventListener("click", function () {
+    getMemoryCost(180);
+})
 
-if(promoInput.value == "stevekaku"){
-    console.log("hello stevekaku");
+////////////////////--------storage button click---------//////////////////////////
+// storage256GB
+document.getElementById("storage256-btn").addEventListener("click", function () {
+    getStorageCost(0);
+})
+// storage512GB
+document.getElementById("storage512-btn").addEventListener("click", function () {
+    getStorageCost(100);
+})
+// storage1000GB
+document.getElementById("storage1000-btn").addEventListener("click", function () {
+    getStorageCost(180);
+})
 
-    totalAmountId.innerText = discountAmount;
-}
-else{
-    console.log("promo code not matched");
-}
+/////////////////////---------delivery button click-------------////////////////////////
 
-promoInput.value = "";
+//delivery25Aug
+document.getElementById("delivery25-btn").addEventListener("click", function () {
+    getDeliveryCost(0);
+})
 
+//delivery25Aug
+document.getElementById("delivery21-btn").addEventListener("click", function () {
+    getDeliveryCost(20);
+})
+
+//------////------------//-- Promo code Check ---//-----------//-------------------
+
+document.getElementById("promo-btn").addEventListener("click", function(){
+    let promoInput = document.getElementById("promo-input");
+    let promoInputValue = promoInput.value;
+    //get total price
+    let totalAmountId = document.getElementById("total-amount");
+    let totalAmount = totalAmountId.innerText;
+
+    //get discount (20%)
+    let discount = (totalAmount * 20) / 100;
+    // Discount Amount
+    let discountAmount = totalAmount - discount;
+
+    //promo code validation check
+    if (promoInput.value == "stevekaku") {
+        console.log("hello stevekaku");
+
+        totalAmountId.innerText = discountAmount;
+    }
+    else {
+        console.log("promo code not matched");
+    }
+    //clear display input value
+    promoInput.value = "";
 
 })
